c61971f1d4b4e88d6c94eb9d67eee6c143e1c62a
Alexandre Bretelle <alexandre.bretelle@ynov.com>
2020-05-12 18:05:18 +0200

    Redirection V1

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,6 +6,18 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  {
+    path: 'pizzas',
+    loadChildren: () => import('../pages/pizzas/pizzas.module').then( m => m.PizzasPageModule)
+  },
+  {
+    path: 'panier',
+    loadChildren: () => import('../pages/panier/panier.module').then( m => m.PanierPageModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('../pages/admin/admin.module').then( m => m.AdminPageModule)
   }
 ];
 
